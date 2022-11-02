d3ee596e282d00111285f44c91bbd19c195b5be9
mifouts <fouts.mitch15@gmail.com>
2022-11-02 17:04:27 -0400

    creating highlights component

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,29 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react'
+
+function Highlights() {
+  return (
+    <section id="highlights">
+        <div className="container">
+            <div className="row">
+             <h2 className="section__title">
+                Why choose <span className="purple">Library</span>? 
+                </h2>
+                <div className="highlight__wrapper">
+                    <div className="highlight">
+                        <div className="highlight__img">
+                            <FontAwesomeIcon icon="bolt"/>
+                        </div>
+                        <h3 className="highlight__subtitle">
+                            Easy and Quick
+                        </h3>
+                        <p className="highlight__para">Get access to the book you purchased online immediately.</p>
+                    </div>
+                </div>   
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Highlights;
