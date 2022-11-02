4bcff40c11bd6a443d983027d5cb9324b1324486
mifouts <fouts.mitch15@gmail.com>
2022-11-02 16:59:51 -0400

    importing landing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Nav from './components/Nav';
+import Landing from './components/Landing';
 
 
 function App() {
   return (
     <div className="App">
   <Nav />
+  <Landing />
     </div>
   );
 }
