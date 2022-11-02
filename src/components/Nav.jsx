852c8e29bc67a8501600fcff5908b76886ef58a7
mifouts <fouts.mitch15@gmail.com>
2022-11-02 14:02:15 -0400

    creating nav component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,28 @@
+import React from "react";  
+
+const Nav = () => {
+    <nav>
+        <div className="nav_container">
+            <a href="/">
+                <img src="" alt="" className='logo'/>
+            </a>
+            <ul className="nav__links">
+                <li className="nav__list">
+                    <a href="/" className="nav__link">
+                        Home
+                    </a>
+                </li>
+                <li className="nav__list">
+                    <a href="/" className="nav__link">
+                        Books
+                    </a>
+                </li>
+                <button className="btn__menu">
+                    <FontAwesomeIcon icon="bars" />
+                </button>
+            </ul>
+        </div>
+    </nav>
+}
+
+export default Nav;
