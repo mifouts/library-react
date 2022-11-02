6c01551de6adfc0e2a6138daf75a67be4804b159
mifouts <fouts.mitch15@gmail.com>
2022-11-02 17:07:38 -0400

    editing highlight component

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -19,6 +19,24 @@ function Highlights() {
                         </h3>
                         <p className="highlight__para">Get access to the book you purchased online immediately.</p>
                     </div>
+                    <div className="highlight">
+                        <div className="highlight__img">
+                            <FontAwesomeIcon icon="book-open"/>
+                        </div>
+                        <h3 className="highlight__subtitle">
+                            10,000+ Books
+                        </h3>
+                        <p className="highlight__para">Library has books in all your favorite categories.</p>
+                    </div>
+                    <div className="highlight">
+                        <div className="highlight__img">
+                            <FontAwesomeIcon icon="tags"/>
+                        </div>
+                        <h3 className="highlight__subtitle">
+                            Affordable
+                        </h3>
+                        <p className="highlight__para">Get your hands on popular books for as little as $10.</p>
+                    </div>
                 </div>   
             </div>
         </div>
