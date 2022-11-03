9c1ce8ccc8c737ad860ca5a6e8642a9f7d9ed38f
mifouts <fouts.mitch15@gmail.com>
2022-11-03 14:42:17 -0400

    discounted section

--- src/components/Discounted.jsx
+++ src/components/Discounted.jsx
@@ -12,7 +12,12 @@ function Discounted() {
                     Discount <span className='purple'>Books</span>
                 </h2>
                 <div className="books">
-                    {books.map((book) => <Book book={book} key={book.id} />)}
+                    {books
+                    .filter(book => book.salePrice > 0)
+                    .slice(0, 8)
+                    .map((book) => (
+                    <Book book={book} key={book.id} />
+                    ))}
                 </div>
             </div>
         </div>
