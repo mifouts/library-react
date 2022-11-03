d66b2463b5f47c3ac16a58c80d34ee9b958fce03
mifouts <fouts.mitch15@gmail.com>
2022-11-03 14:33:58 -0400

    creating new discounted books component

--- src/components/ui/book.jsx
+++ src/components/ui/book.jsx
@@ -1,39 +1,38 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const book = ({ book }) => {
   return (
     <div className="book">
-                        <a href="/">
-                            <figure className='book__img--wrapper'>
-                                <img src={book.url} alt="" className='book__img' />
-                            </figure>
-                        </a>
-                        <div className="book__title">
-                            <a href="/" className='book__title--link'>
-                                {book.title}
-                            </a>
-                        </div>
-                        <div className="book__ratings">
-                            <FontAwesomeIcon icon='star'/>
-                            <FontAwesomeIcon icon='star'/>
-                            <FontAwesomeIcon icon='star'/>
-                            <FontAwesomeIcon icon='star'/>
-                            <FontAwesomeIcon icon='star-half-alt'/>
-                        </div>
-                        <div className="book__price">
-                            {book.salePrice ? ( 
-                                <>
-                                <span className="book__price--normal">${book.originalPrice}</span>
-                                ${book.salePrice}
-                                </>
-                                ) : (
-                                   book.originalPrice
-                                )}
-                            
-                        </div>
-                    </div>
-  )
-}
+      <a href="/">
+        <figure className="book__img--wrapper">
+          <img src={book.url} alt="" className="book__img" />
+        </figure>
+      </a>
+      <div className="book__title">
+        <a href="/" className="book__title--link">
+          {book.title}
+        </a>
+      </div>
+      <div className="book__ratings">
+        {
+            new Array(5).fill(0).map((_, index) => <FontAwesomeIcon icon='star' key={index}/>)
+        }
+      </div>
+      <div className="book__price">
+        {book.salePrice ? (
+          <>
+            <span className="book__price--normal">
+              ${book.originalPrice.toFixed(2)}
+            </span>
+            ${book.salePrice.toFixed(2)}
+          </>
+        ) : (
+          <>${book.originalPrice.toFixed(2)}</>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default book;
