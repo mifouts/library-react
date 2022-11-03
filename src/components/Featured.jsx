11436b98e945781ea1574b3d03e0fc75287a57bc
mifouts <fouts.mitch15@gmail.com>
2022-11-03 14:00:42 -0400

    creating book component

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,5 +1,5 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
+import book from './ui/book';
 
 const Featured = () => {
     return(
@@ -10,29 +10,7 @@ const Featured = () => {
                     Featured <span className='purple'>Books</span>
                 </h2>
                 <div className="books">
-                    <div className="book">
-                        <a href="/">
-                            <figure className='book__img--wrapper'>
-                                <img src="https://covers.openlibrary.org/b/id/10958382-L.jpg" alt="" className='book__img' />
-                            </figure>
-                        </a>
-                        <div className="book__title">
-                            <a href="/" className='book__title--link'>
-                                Atomic Habits
-                            </a>
-                        </div>
-                        <div className="book__ratings">
-                            <FontAwesomeIcon icon='star'/>
-                            <FontAwesomeIcon icon='star'/>
-                            <FontAwesomeIcon icon='star'/>
-                            <FontAwesomeIcon icon='star'/>
-                            <FontAwesomeIcon icon='star-half-alt'/>
-                        </div>
-                        <div className="book__price">
-                            <span className="book__price--normal">$15.00</span>
-                            $10.00
-                        </div>
-                    </div>
+                    <book />
                 </div>
             </div>
         </div>
