53b207bcd85f75d035faff70da2deefb99859d4b
mifouts <fouts.mitch15@gmail.com>
2022-11-04 15:14:15 -0400

    finishing footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,6 +11,15 @@ export default function Footer() {
                         <img src={Logo} className='footer__logo--img' alt="" />
                     </figure>
                 </a>
+                <div className="footer__list">
+                    <a href="/" className='footer__link'>
+                      Home  
+                    </a>
+                    <span className='footer__link no__cursor'>About</span>
+                    <a href="/books" className='footer__link'>Books</a>
+                    <a href="/cart" className='footer__link'>Cart</a>
+                </div>
+                <div className="footer__copyright">Copyright &copy; 2022 Library</div>
             </div>
         </div>
     </footer>
