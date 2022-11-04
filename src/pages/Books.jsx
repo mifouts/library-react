9e68c773d031240f95b223fb0bf29cc710691133
mifouts <fouts.mitch15@gmail.com>
2022-11-04 16:17:44 -0400

    filtering

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Book from '../components/ui/Book';
 
-export default function Books({ books }) {
+
+export default function Books({ books: initialBooks }) {
+    const [books, setBooks] = useState(initialBooks);
+
+    function filterBooks(filter) {
+        console.log(filter)
+    }
   return (
     <div id="books__body">
         <main id="books__main">
@@ -11,7 +17,7 @@ export default function Books({ books }) {
                         <div className="books__header">
                             <h2 className='section__title books__header--title'>
                                 All Books</h2>
-                                <select name="" id="filter" defaultValue="DEFAULT">
+                                <select name="" id="filter" defaultValue="DEFAULT" onChange={(event) =>filterBooks(event.target.value)}>
                                     <option value="DEFAULT" selected disabled>Sort</option>
                                     <option value="LOW_TO_HIGH">Price, Low to High</option>
                                     <option value="HIGH_TO_LOW">Price, High to Low</option>
