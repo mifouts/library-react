e5a75becca22487708458e9303cfd1e4b93f04d9
mifouts <fouts.mitch15@gmail.com>
2022-11-04 15:29:31 -0400

    creating home component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import Nav from './components/Nav';
-import Landing from './components/Landing';
-import Highlights from './components/Highlights';
-import Featured from './components/Featured';
-import Discounted from './components/Discounted';
-import Explore from './components/Explore';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
+      <Route />
   <Nav />
-  <Landing />
-  <Highlights />
-  <Featured />
-  <Discounted />
-  <Explore />
+  
   <Footer />
     </div>
+    </Router>
   );
 }
 
