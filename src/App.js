5b45a15cfb966c394f46ea07a7cffa00895bf7fc
mifouts <fouts.mitch15@gmail.com>
2022-11-04 15:32:08 -0400

    routing home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './pages/Home';
+
 
 function App() {
   return (
     <Router>
     <div className="App">
-      <Route />
   <Nav />
-  
+  <Route path="/" exact component={Home}/>
   <Footer />
     </div>
     </Router>
