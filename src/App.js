7f80a298fab02246d4f0c5f2a4ac8b31f9808e08
mifouts <fouts.mitch15@gmail.com>
2022-11-05 14:36:30 -0400

    book info

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Books from './pages/Books';
 import { books } from './data';
+import BookInfo from './pages/BookInfo';
 
 function App() {
   return (
