e8380b129674a46be3f44bef3106b5784edc37c6
mifouts <fouts.mitch15@gmail.com>
2022-11-07 14:23:30 -0500

    creating book info pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Nav from './components/Nav';
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Books from './pages/Books';
 import { books } from './data';
@@ -11,12 +11,10 @@ function App() {
     <Router>
     <div className="App">
   <Nav />
-  <Routes>
-  <Route path="/" element={<Home />}/>
-  <Route path="/books" element={<Books books={books} />} />
-  <Route path="/books/:id" element={<BookInfo books={books} />} />
+  <Route path="/" exact component={Home}/>
+  <Route path="/books" exact render={() => <Books books={books} />} />
+  <Route path="/books/:id" render={() => <BookInfo books={books} />} />
   <Footer />
-  </Routes>
     </div>
     </Router>
   );
--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,4 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 
 export default function BookInfo({ books }) {
   return (
@@ -7,7 +9,9 @@ export default function BookInfo({ books }) {
             <div className="books__container">
                 <div className="row">
                     <div className="book__selected--top">
-                        
+                        <Link to="/books" className="book__link">
+                              <FontAwesomeIcon icon="arrow-left"/>
+                        </Link>
                     </div>
                 </div>
             </div>
