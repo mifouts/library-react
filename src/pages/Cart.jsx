f188aca10c716ad0395d8e37a039c65b2197ea4c
mifouts <fouts.mitch15@gmail.com>
2022-11-12 16:05:46 -0500

    creating cart page

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Cart() {
+  return (
+    <div id="books__body">
+        <main id="books__main">
+            <div className="books__container">
+                <div className="row">
+                    <div className="book__selected--top">
+                        <h2 className="cart__title">Cart</h2>
+                    </div>
+                    <div className="cart">
+                        <div className="cart__header">
+                            <div className="cart__book">Book</div>
+                            <div className="cart__quantity">Book</div>
+                            <div className="cart__total">Book</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </main>
+    </div>
+  )
+}
