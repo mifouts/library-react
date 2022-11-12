437a5a51d9e92572489021773b27fc5ef37e7579
mifouts <fouts.mitch15@gmail.com>
2022-11-12 15:58:18 -0500

    inserting proper class name

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -64,12 +64,13 @@ export default function BookInfo({ books }) {
                 Recommended Books
               </h2>
             </div>
+            <div className="books">
            {books
            .filter((book) => book.rating === 5 && book.id !== id )
            .slice(0, 4)
             .map((book) => (<Book book={book} key={book.id} />
               ))} 
-            
+            </div>
           </div>
         </div>
       </main>
