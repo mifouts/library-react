96abe5a7b6564a250ee932c1f8276d703eb8f980
mifouts <fouts.mitch15@gmail.com>
2022-11-15 14:38:08 -0500

    making books dynamic

--- src/components/ui/book.jsx
+++ src/components/ui/book.jsx
@@ -6,18 +6,18 @@ import Rating from "./Rating";
 const Book = ({ book }) => {
   return (
     <div className="book">
-      <Link to="/books/1">
+      <Link to={`/books/${book.id}`}>
         <figure className="book__img--wrapper">
           <img src={book.url} alt="" className="book__img" />
         </figure>
       </Link>
       <div className="book__title">
-        <Link to="/books/1" className="book__title--link">
+        <Link to={`/books/${book.id}`} className="book__title--link">
           {book.title}
         </Link>
       </div>
       <Rating rating={book.rating}/>
-      <Price />
+      <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
     </div>
   );
 };
