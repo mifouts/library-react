efd82ed54685ee8ba5e926a5cd67b779435d07e6
mifouts <fouts.mitch15@gmail.com>
2022-11-15 15:17:40 -0500

    doing add to cart a different way

--- src/App.js
+++ src/App.js
@@ -13,25 +13,7 @@ function App() {
   const [cart, setCart] = useState([]);
 
   function addToCart(book) {
-    const dupeItem = cart.find(item => +item.id === +book.id)
-    if (dupeItem) {
-    dupeItem.quantity += 1;
-    setCart(cart.map((item) => {
-     if (item.id === dupeItem.id) {
-      return {
-        ...item,
-        quantity: item.quantity + 1,  
-      };
-     }
-     else {
-      return item;
-     }
-    })
-    );
-    }
-    else {
-      setCart([...cart, {...book, quantity: 1}]);
-    }
+    setCart([book])
   }
 
   useEffect(() => {
