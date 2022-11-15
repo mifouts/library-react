e85c3f3c6edd150610fc86af4b82703290922e53
mifouts <fouts.mitch15@gmail.com>
2022-11-15 15:12:46 -0500

    finishing touches on add to cart

--- src/App.js
+++ src/App.js
@@ -16,17 +16,18 @@ function App() {
     const dupeItem = cart.find(item => +item.id === +book.id)
     if (dupeItem) {
     dupeItem.quantity += 1;
-    setCart(cart.map(item => {
+    setCart(cart.map((item) => {
      if (item.id === dupeItem.id) {
       return {
         ...item,
-        quantity: item.quantity +1,  
-      }
+        quantity: item.quantity + 1,  
+      };
      }
      else {
-      return item
+      return item;
      }
-    }))
+    })
+    );
     }
     else {
       setCart([...cart, {...book, quantity: 1}]);
