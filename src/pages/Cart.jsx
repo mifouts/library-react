1b42eef42cc7d2410578f0eacb48d3eb10d97be6
mifouts <fouts.mitch15@gmail.com>
2022-11-16 17:16:56 -0500

    making cart dynamic

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,66 +1,71 @@
-import React from 'react'
+import React from "react";
 
-export default function Cart() {
+export default function Cart({ cart }) {
   return (
     <div id="books__body">
-        <main id="books__main">
-            <div className="books__container">
-                <div className="row">
-                    <div className="book__selected--top">
-                        <h2 className="cart__title">Cart</h2>
-                    </div>
-                    <div className="cart">
-                        <div className="cart__header">
-                            <span className="cart__book">Book</span>
-                            <span className="cart__quantity">Quantity</span>
-                            <span className="cart__total">Price</span>
-                        </div>
-                        <div className="cart__body">
-                            <div className="cart__item">
-                                <div className="cart__book">
-                                    <img src="" className='cart__book--img' alt="" />
-                                    <div className="cart__book--info">
-                                        <span className="cart__book--title">
-                                            Crack the Code
-                                        </span>
-                                        <span className="cart__book--price">
-                                            $10,000
-                                        </span>
-                                        <button className="cart__book--remove">
-                                            Remove
-                                        </button>
-                                    </div>
-                                </div>
-                                <div className="cart__quantity">
-                                    <input type="number" min={0} max={99} className='cart__input' />
-                                </div>
-                                <div className="cart__total">
-                                    $10
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="total">
-                        <div className="total__item total__sub-total">
-                            <span>Subtotal</span>
-                            <span>$9</span>
-                        </div>
-                        <div className="total__item total__tax">
-                            <span>Tax</span>
-                            <span>$1</span>
-                        </div>
-                        <div className="total__item total__price">
-                            <span>Total</span>
-                            <span>$10</span>
-                        </div>
-                        <button className="btn btn__checkout no-cursor"
-                        onClick={() => alert('have not got around to doing this')}>
-                            Proceed to Checkout
-                        </button>
-                    </div>
-                </div>
+      <main id="books__main">
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <h2 className="cart__title">Cart</h2>
             </div>
-            </main>
+            <div className="cart">
+              <div className="cart__header">
+                <span className="cart__book">Book</span>
+                <span className="cart__quantity">Quantity</span>
+                <span className="cart__total">Price</span>
+              </div>
+              <div className="cart__body">
+                {cart.map((book) => {
+                  return (
+                    <div className="cart__item">
+                      <div className="cart__book">
+                        <img src="" className="cart__book--img" alt="" />
+                        <div className="cart__book--info">
+                          <span className="cart__book--title">
+                            Crack the Code
+                          </span>
+                          <span className="cart__book--price">$10,000</span>
+                          <button className="cart__book--remove">Remove</button>
+                        </div>
+                      </div>
+                      <div className="cart__quantity">
+                        <input
+                          type="number"
+                          min={0}
+                          max={99}
+                          className="cart__input"
+                        />
+                      </div>
+                      <div className="cart__total">$10</div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="total">
+              <div className="total__item total__sub-total">
+                <span>Subtotal</span>
+                <span>$9</span>
+              </div>
+              <div className="total__item total__tax">
+                <span>Tax</span>
+                <span>$1</span>
+              </div>
+              <div className="total__item total__price">
+                <span>Total</span>
+                <span>$10</span>
+              </div>
+              <button
+                className="btn btn__checkout no-cursor"
+                onClick={() => alert("have not got around to doing this")}
+              >
+                Proceed to Checkout
+              </button>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
-  )
+  );
 }
