ae0bae51b21e91b67e1a86acdbb0225596c34fff
mifouts <fouts.mitch15@gmail.com>
2022-11-19 14:48:02 -0500

    only showing empty state when cart is empty

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import EmptyCart from "../assets/empty_cart.svg"
-
+import EmptyCart from "../assets/empty_cart.svg";
 
 export default function Cart({ cart, changeQuantity, removeItem }) {
-        const total = () => {
-            let price = 0;
-            cart.forEach((item) => {
-                price += +((item.salePrice || item.originalPrice) * item.quantity);
-            });
-                return price;
-        }
+  const total = () => {
+    let price = 0;
+    cart.forEach((item) => {
+      price += +((item.salePrice || item.originalPrice) * item.quantity);
+    });
+    return price;
+  };
   return (
     <div id="books__body">
       <main id="books__main">
@@ -30,13 +29,24 @@ export default function Cart({ cart, changeQuantity, removeItem }) {
                   return (
                     <div className="cart__item">
                       <div className="cart__book">
-                        <img src={book.url} className="cart__book--img" alt="" />
+                        <img
+                          src={book.url}
+                          className="cart__book--img"
+                          alt=""
+                        />
                         <div className="cart__book--info">
                           <span className="cart__book--title">
                             {book.title}
                           </span>
-                          <span className="cart__book--price">${(book.salePrice || book.originalPrice).toFixed(2) }</span>
-                          <button className="cart__book--remove" onClick={() => removeItem(book)}>Remove</button>
+                          <span className="cart__book--price">
+                            ${(book.salePrice || book.originalPrice).toFixed(2)}
+                          </span>
+                          <button
+                            className="cart__book--remove"
+                            onClick={() => removeItem(book)}
+                          >
+                            Remove
+                          </button>
                         </div>
                       </div>
                       <div className="cart__quantity">
@@ -46,43 +56,52 @@ export default function Cart({ cart, changeQuantity, removeItem }) {
                           max={99}
                           className="cart__input"
                           value={book.quantity}
-                          onChange={(event) => changeQuantity(book, event.target.value)}
+                          onChange={(event) =>
+                            changeQuantity(book, event.target.value)
+                          }
                         />
                       </div>
-                      <div className="cart__total">${((book.salePrice || book.originalPrice) * book.quantity).toFixed(2)}</div>
+                      <div className="cart__total">
+                        $
+                        {(
+                          (book.salePrice || book.originalPrice) * book.quantity
+                        ).toFixed(2)}
+                      </div>
                     </div>
                   );
                 })}
               </div>
-              <div className="cart__empty">
-                <img src={EmptyCart} alt="" className="cart__empty--img" />
-                <h2>You don't have any books in your cart.</h2>
-                <Link to="/books">
-                <button className="btn">Browse Books</button>
-                </Link>
-              </div>
+              {cart.length === 0 && (
+                <div className="cart__empty">
+                  <img src={EmptyCart} alt="" className="cart__empty--img" />
+                  <h2>You don't have any books in your cart.</h2>
+                  <Link to="/books">
+                    <button className="btn">Browse Books</button>
+                  </Link>
+                </div>
+              )}
             </div>
             {cart.length > 0 && (
-            <div className="total">
-              <div className="total__item total__sub-total">
-                <span>Subtotal</span>
-                <span>${(total() * 0.9).toFixed(2)}</span>
+              <div className="total">
+                <div className="total__item total__sub-total">
+                  <span>Subtotal</span>
+                  <span>${(total() * 0.9).toFixed(2)}</span>
+                </div>
+                <div className="total__item total__tax">
+                  <span>Tax</span>
+                  <span>${(total() * 0.1).toFixed(2)}</span>
+                </div>
+                <div className="total__item total__price">
+                  <span>Total</span>
+                  <span>${total().toFixed(2)}</span>
+                </div>
+                <button
+                  className="btn btn__checkout no-cursor"
+                  onClick={() => alert("have not got around to doing this")}
+                >
+                  Proceed to Checkout
+                </button>
               </div>
-              <div className="total__item total__tax">
-                <span>Tax</span>
-                <span>${(total() * 0.1).toFixed(2)}</span>
-              </div>
-              <div className="total__item total__price">
-                <span>Total</span>
-                <span>${total().toFixed(2)}</span>
-              </div>
-              <button
-                className="btn btn__checkout no-cursor"
-                onClick={() => alert("have not got around to doing this")}
-              >
-                Proceed to Checkout
-              </button>
-            </div>
             )}
           </div>
         </div>
