f881f1e63567044e7376300171d44b717d37b809
mifouts <fouts.mitch15@gmail.com>
2022-11-19 14:52:05 -0500

    making cart counter

--- src/App.js
+++ src/App.js
@@ -33,6 +33,14 @@ function App() {
     setCart(cart.filter((book) => book.id !== item.id))
   }
 
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach((item) =>{
+    counter += item.quantity
+    })
+    return counter;
+  }
+
   useEffect(() => {
   console.log(cart);
 }, [cart])
@@ -40,7 +48,7 @@ function App() {
   return (
     <Router>
     <div className="App">
-  <Nav />
+  <Nav numberOfItems={numberOfItems}/>
   <Route path="/" exact component={Home}/>
   <Route path="/books" exact render={() => <Books books={books} />} />
   <Route path="/books/:id" render={() => <BookInfo books={books} addToCart={addToCart} cart={cart} />} />
